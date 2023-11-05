59ec914e02938dc0446f6679e5272d8033e6f66b
Martin Dorazil <mmdorazil@gmail.com>
2023-11-05 21:10:19 +0100

    Initial commit

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+ml64 /nologo /Zi /c count_to_n.asm
+link /nologo /DEBUG /MACHINE:x64 /OUT:count_to_n.exe /ENTRY:main count_to_n.obj kernel32.lib user32.lib legacy_stdio_definitions.lib ucrt.lib msvcrt.lib
+
+ml64 /nologo /Zi /c count.asm
+link /nologo /DEBUG /MACHINE:x64 /OUT:count.exe /ENTRY:main count.obj kernel32.lib user32.lib legacy_stdio_definitions.lib ucrt.lib msvcrt.lib
---
+++ count.asm
@@ -0,0 +1,91 @@
+_CRT_INIT   proto
+;-------------------------------------------------------------------------------
+; Experimental program reading the input and printing out in loop.
+;-------------------------------------------------------------------------------
+
+ExitProcess proto
+printf      proto
+scanf       proto
+
+;-------------------------------------------------------------------------------
+.data
+
+get_chars_msg db "Insert number: ", 0
+msg           db "i = %d", 0Ah, 0
+scan_str      db "%d", 0
+
+;-------------------------------------------------------------------------------
+.code
+
+;- main ------------------------------------------------------------------------
+main proc
+	push    rbp
+	mov     rbp, rsp
+
+;- LOCALS ----------------------------------------------------------------------
+; int n = 0
+	sub     rsp, 10h
+	mov     dword ptr [rbp+04h], 0
+;-------------------------------------------------------------------------------
+
+	sub     rsp, 20h ; add shadow space
+	call    _CRT_INIT
+
+	lea     rcx, [get_chars_msg]
+	call    printf
+
+	lea     rcx, [scan_str]
+	lea     rdx, [rbp+04h]
+	call    scanf
+
+	mov     ecx, dword ptr [rbp+04h]
+	call    print_hello
+
+	mov     rcx, 0
+	call    ExitProcess
+	add     rsp, 20h
+	ret
+main endp
+;-------------------------------------------------------------------------------
+
+;- print_hello -----------------------------------------------------------------
+; ecx - count of hello messages
+;-------------------------------------------------------------------------------
+print_hello proc
+	push    rbp
+	mov     rbp, rsp
+
+;- LOCALS ----------------------------------------------------------------------
+; int c = 0
+	sub     rsp, 10h
+	mov     dword ptr [rbp+04h], 0
+;-------------------------------------------------------------------------------
+
+	sub     rsp, 20h
+
+	mov     dword ptr [rbp+10h], ecx
+
+_next:
+	mov     eax, dword ptr [rbp+10h]
+	cmp     dword ptr [rbp+04h], eax
+	je      _done
+
+	lea     rcx, [msg]
+	mov     edx, dword ptr [rbp+04h]
+	call    printf
+
+	inc     dword ptr [rbp+04h]
+	jmp     _next
+
+_done:
+	xor     eax, eax
+	; clanup local variables
+	add     rsp, 10h
+
+	add     rsp, 20h
+	pop     rbp
+	ret
+print_hello endp
+;-------------------------------------------------------------------------------
+
+end
